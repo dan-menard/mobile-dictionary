b0a9bb44534f8291b80d8be1536227e3f4a7a1ce
Dan Menard <dan.menard@shopify.com>
2020-12-19 11:03:14 -0500

    Create Swift app.

---
+++ Swift Dictionary/Swift Dictionary/Swift_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DictionaryApp.swift
+//  Swift Dictionary
+//
+//  Created by Dan Menard on 2020-12-19.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
