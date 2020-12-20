92216f3b34ccd705a010e8bd30f5fb23ff67e3c5
Dan Menard <dan.menard@shopify.com>
2020-12-20 09:33:48 -0500

    Listen on a second port, and return some mock json for now.

--- api-proxy/proxy.js
+++ api-proxy/proxy.js
@@ -2,32 +2,43 @@ const http = require('http');
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const proxyPort = 3001;
+const uiPort = 3000;
 
-function startServer(html) {
+function startProxyServer() {
+  const jsonPayload = JSON.stringify({hello: 'world'});
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(jsonPayload);
+  });
+
+  server.listen(proxyPort, hostname, () => {
+    console.log(`Proxy running at http://${hostname}:${proxyPort}/`);
+  });
+}
+
+function startUiServer(html) {
   const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-
-    if (html) {
-      res.write(html);
-    }
-
+    res.write(html);
     res.end();
   });
 
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  server.listen(uiPort, hostname, () => {
+    console.log(`UI running at http://${hostname}:${uiPort}/`);
   });
 }
 
 fs.readFile('api-proxy/proxy-ui.html', (err, html) => {
   if (err) {
-    console.log('ooh! Something went wrong loading the HTML. Starting with no UI.');
+    console.log('ooh! Something went wrong loading the HTMLL:');
     console.log(err);
-
-    startServer();
   } else {
-    startServer(html);
+    startUiServer(html);
   }
 });
+
+startProxyServer();
