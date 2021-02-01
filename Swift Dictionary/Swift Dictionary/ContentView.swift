d9191202ca33f2f54f7ed2b68d994c92779aab76
Dan Menard <dan.menard@shopify.com>
2021-02-01 13:26:56 -0500

    Add data fetching in Swift. Just print the data description for now, to verify it's fetching stuff.

--- Swift Dictionary/Swift Dictionary/ContentView.swift
+++ Swift Dictionary/Swift Dictionary/ContentView.swift
@@ -8,26 +8,56 @@
 import SwiftUI
 
 struct SearchView: View {
-    @State private var word: String = "";
+    @State private var word: String = ""
+    @State private var loadResults: Int? = 0
+    @State private var wordData: Data = Data.init()
     
     var body: some View {
         NavigationView {
             VStack {
                 TextField("Enter a word", text: $word)
-                NavigationLink(destination: DefinitionView(word: word)) {
-                    Text("Search")
+                NavigationLink(
+                    destination: DefinitionView(word: word, data: wordData),
+                    tag: 1,
+                    selection: $loadResults
+                ) {
+                    Text("Search").onTapGesture {
+                        self.fetchResults(word: word);
+                    }
                 }.buttonStyle(PlainButtonStyle())
             }
         }
     }
+    
+    func fetchResults(word: String) {
+        let url = URL(string: "http://127.0.0.1:3001/?word=\(word)")!
+        let req = URLRequest(url: url)
+        
+        let session = URLSession.shared;
+        let task = session.dataTask(with: req) {
+            (data, response, error) in
+                if let error = error {
+                    // TODO
+                } else if let data = data {
+                    wordData = data;
+                    loadResults = 1;
+                } else {
+                    // Something weird happened
+                }
+        }
+        
+        task.resume()
+    }
 }
 
 struct DefinitionView: View {
-    let word: String;
+    let word: String
+    let data: Data
     
     var body: some View {
         VStack {
             Text("Definition page for \(word)")
+            Text("Also \(data.description)")
         }
     }
 }
